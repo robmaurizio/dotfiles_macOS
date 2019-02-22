5c667d9c9f00fbc125a5703a17f3a5275bc6d628
robmaurizio <rob.maurizio@gmail.com>
2019-02-22 13:33:36 -0600

    script to update brew, macOS, and run cleanup

---
+++ dotfiles/Software update.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+print("\nChecking for software updates via Brew.\n")
+
+# Updates Homebrew, taps, and repos
+os.system('brew update ; brew upgrade ; brew cask upgrade')
+
+print('\nBrew updates are done. Now doing some cleanup.')
+
+# Pauses the script before cleaning up
+time.sleep(1)
+
+# Cleanup after running updates
+os.system('brew cleanup ; brew cleanup -s')
+
+print('Cleanup is done. Checking for macOS updates.\n')
+
+# Pauses the script
+time.sleep(1)
+
+# Checks for macOS updates and install all available
+os.system('softwareupdate -ia')
+
+print('\nFinished! Your system is ready.\n')
