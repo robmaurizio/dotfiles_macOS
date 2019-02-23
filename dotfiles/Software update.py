06eca2812bf162dd457b2ecaa18c3b791904b2ac
Rob Maurizio <rob.maurizio@gmail.com>
2019-02-23 09:34:17 -0600

    /Users/rob/dotfiles/Software update.py

--- dotfiles/Software update.py
+++ dotfiles/Software update.py
@@ -3,20 +3,20 @@
 import os
 import time
 
-print("\nChecking for software updates via Brew.\n")
+print("\nChecking for software updates via Brew...\n")
 
 # Updates Homebrew, taps, and repos
 os.system('brew update ; brew upgrade ; brew cask upgrade')
 
-print('\nBrew updates are done. Now doing some cleanup.')
+print('\nBrew updates are done. Now for some cleanup...')
 
 # Pauses the script before cleaning up
 time.sleep(1)
 
 # Cleanup after running updates
-os.system('brew cleanup ; brew cleanup -s ; cleanupds')
+os.system('brew cleanup ; brew cleanup -s')
 
-print('Cleanup is done. Checking for macOS updates.\n')
+print('Cleanup is done. Checking for macOS updates...\n')
 
 # Pauses the script
 time.sleep(1)
