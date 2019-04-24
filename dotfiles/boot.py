d2be95938ec77f3ed177398ae4432578cb838d14
Rob Maurizio <45679381+robmaurizio@users.noreply.github.com>
2019-04-24 15:51:54 -0500

    Add files via upload
    gettin' it started

---
+++ dotfiles/boot.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('open -a "Google Chrome"')
+
+os.system('open -a "Slack"')
+
+os.system('open -a "Microsoft Remote Desktop Beta"')
+
+os.system('python ~/dotfiles/Software\ update.py')
