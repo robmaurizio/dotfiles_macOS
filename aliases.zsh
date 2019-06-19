cd313f0fba6c8c96730d9922a42e3c8effabd2cd
Rob Maurizio <45679381+robmaurizio@users.noreply.github.com>
2019-06-19 15:52:44 +0000

    Update and rename .aliases to aliases.zsh
    changed all calls to the user's home directory from '~' to '/Users/$USER' to make the files more portable

--- aliases.zsh
+++ aliases.zsh
@@ -3,26 +3,26 @@
 ##################
 #   Navigation   #
 ##################
-alias ..="cd .."
-alias cd..="cd .."
-alias c="clear"
+alias ..='cd ..'
+alias cd..='cd ..'
+alias c='clear'
 alias q='exit'
 alias ls='ls -FG' # List files in the current directory
 alias la='ls -aFG' # List (all) files in the current directory
 alias ll='ls -l' # List files as a list in the current directory
-alias ld="ls -ld */"   # List files in long format, only directories
+alias ld='ls -ld */'   # List files in long format, only directories
 
 ###################
 #   Directories   #
 ###################
 alias apps='cd /Applications'
-alias gd='cd ~/Google\ Drive/'
-alias pydir='cd ~/Google\ Drive/Python'
-alias library="cd ~/Library"
-alias dt='cd ~/Desktop/'
-alias dl='cd ~/Downloads/'
+alias gd='cd /Users/$USER/Google\ Drive/'
+alias pydir='cd /Users/$USER/Google\ Drive/Python'
+alias library='cd /Users/$USER/Library'
+alias dt='cd /Users/$USER/Desktop/'
+alias dl='cd /Users/$USER/Downloads/'
 alias hs='history | grep'
-alias grep="grep --color=always" # Show results of search in color
+alias grep='grep --color=always' # Show results of search in color
 
 ###################
 #    Operations   #
@@ -30,26 +30,26 @@ alias grep="grep --color=always" # Show results of search in color
 alias mv='mv -i' # Move -interactive
 alias cp='cp -i' # Copy -interactive
 alias rm='rm -i' # Remove (delete) -interactive
-alias empty="sudo rm -rf ~/.Trash/*" # Take out the trash
+alias empty='sudo rm -rf /Users/$USER/.Trash/*' # Take out the trash
 
 ###################
 #     System      #
 ###################
-alias boot='python ~/scripts/boot.py'
+alias boot='python /Users/$USER/scripts/boot.py'
 alias ps='ps -acmx'
 alias root='sudo -i' # Allow root access
 alias rebootnow='sudo shutdown -r now'
 alias sleepnow='pmset sleepnow'
-alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
 alias sudo='sudo ' # Make aliases sudo-able
-alias zshrc='edit ~/.zshrc'
-alias grep="grep -Hn --color=auto" # Show results of search in color
+alias zshrc='edit /Users/$USER/.zshrc'
+alias grep='grep -Hn --color=auto' # Show results of search in color
 
 ###################
 #     Updates     #
 ###################
-alias update='python ~/scripts/softwareUpdate.py' # Look for application and macOS updates
-alias reload='source ~/.zshrc'
+alias update='python /Users/$USER/scripts/softwareUpdate.py' # Look for application and macOS updates
+alias reload='source /Users/$USER/.zshrc'
 alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
 
 ###################
