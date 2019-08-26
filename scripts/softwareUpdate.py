3d3d64b35d0c641b99dfb439e441b65dd1005d20
Rob Maurizio <45679381+robmaurizio@users.noreply.github.com>
2019-08-26 12:31:08 -0500

    Update softwareUpdate.py

--- scripts/softwareUpdate.py
+++ scripts/softwareUpdate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import os
-import time
 
 print('\nChecking for software updates via Brew...')
 
@@ -10,17 +9,11 @@ os.system('brew update ; brew upgrade ; brew cask upgrade')
 
 print('\nUpdates are done. Now for some cleanup...')
 
-# Pauses the script before cleaning up
-time.sleep(1)
-
 # Cleanup after running updates
 os.system('brew cleanup ; brew cleanup -s')
 os.system("find . -type f -name '*.DS_Store' -ls -delete")
 
 print('Cleanup is done. Checking for macOS updates...\n')
-
-# Pauses the script
-time.sleep(1)
 
 # Checks for macOS updates and install all available
 os.system('softwareupdate -ia')
