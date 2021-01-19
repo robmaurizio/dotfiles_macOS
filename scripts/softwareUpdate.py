f64e260183509daabe1897f515a1282c7acefe77
Rob Maurizio <45679381+robmaurizio@users.noreply.github.com>
2021-01-19 09:30:05 -0600

    cleared out some redunancies

--- scripts/softwareUpdate.py
+++ scripts/softwareUpdate.py
@@ -10,14 +10,12 @@ os.system('brew update ; brew upgrade ; brew upgrade --cask')
 print('\nUpdates are done. Now for some cleanup...')
 
 # Cleanup after running updates
-os.system('brew cleanup ; brew cleanup -s')
+os.system('brew cleanup')
 os.system("find . -type f -name '*.DS_Store' -ls -delete")
 
 print('Cleanup is done. Checking for macOS updates...\n')
 
 # Checks for macOS updates and install all available
-os.system('softwareupdate -ia')
+os.system('softwareupdate --install --all')
 
-print('\nFinished! Your system is ready.\n')
-
-os.system('exit')
+print('\nFinished!')
